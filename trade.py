ecc6dd08f729797c33774cf6bcef4d11c795e3c6
TafaraBean <kanyembat25@gmail.com>
2024-06-11 00:07:14 +0200

    environment variable add

--- trade.py
+++ trade.py
@@ -1,8 +1,16 @@
 import time
 from trading_bot import TradingBot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+account=int(os.environ.get("ACCOUNT"))
+password=os.environ.get("PASSWORD")
+server=os.environ.get("SERVER")
 
 
 bot = TradingBot( account=account, password=password, server=server)
 
 
-print(bot.get_position(ticket=18610280))
+
+
